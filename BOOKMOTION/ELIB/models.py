ad9f604d403a0d80c1616c9ae21982a48738f22f
Immiebirungi <immaculatebirungi24@gmail.com>
2022-07-01 16:13:14 +0300

    addition to models

--- BOOKMOTION/ELIB/models.py
+++ BOOKMOTION/ELIB/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Genre(models.Model):
@@ -13,6 +14,8 @@ class Genre(models.Model):
 
 class Author(models.Model):
     name = models.CharField(verbose_name="Author's Name", max_length=100)
+    added_on = models.DateField(verbose_name="Added On",default=timezone.now)
+
 
     def __str__(self):
         return self.name
@@ -22,9 +25,12 @@ class Author(models.Model):
 
 
 class Books(models.Model):
-    title = models.CharField(max_length=150)
-    author = models.ForeignKey(Author,on_delete=models.CASCADE,)
+    title = models.CharField("Title",max_length=150)
+    author = models.ForeignKey(to=Author,on_delete=models.CASCADE,)
     genres = models.ManyToManyField(Genre)
+    is_available = models.BooleanField(verbose_name="Is Available",default=True)
+    added_on = models.DateField(verbose_name="Added On",default=timezone.now)
+
 
     def __str__(self):
         return self.title
