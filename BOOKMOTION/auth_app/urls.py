b0ebfc384b0df1ad3b0093850962074b25e92e65
Luwandagga Mark <markluiz60@gmail.com>
2022-07-20 13:05:38 +0300

    auth app modifications

---
+++ BOOKMOTION/auth_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from auth_app import views
+
+app_name = 'auth_app'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    # student
+    path('register-student/', views.register, name='register-student'),
+    path('login-student/', views.login_student, name='login-student'),
+    path('logout/', views.logout_user, name='logout'),
+
+    # staff
+    path('login-staff/', views.login_staff, name='login-staff'),
+    ]
