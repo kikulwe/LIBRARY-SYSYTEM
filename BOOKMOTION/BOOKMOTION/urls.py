7ee528b002d3dfcc8cb154bc726d350241ce8edd
Immiebirungi <immaculatebirungi24@gmail.com>
2022-07-25 10:48:44 +0300

    book motion urls

--- BOOKMOTION/BOOKMOTION/urls.py
+++ BOOKMOTION/BOOKMOTION/urls.py
@@ -16,6 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include,path
 
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
+    path('', include('elib.urls', namespace='elib')),
+    path('auth/', include('auth_app.urls', namespace='auth_app')),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
